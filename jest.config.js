b8d6dca46c0826b57adb86975d48f5e2b5c30bcf
orozco92 <goldstein.david92@gmail.com>
2023-12-10 18:26:04 -0500

    Project setup
    * package json config
    * TS config
    * Test setup
    * gitignore
    * github test workflow
    * editorconfig
    * Folder structure

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: "node",
+  transform: { "^.+\\.ts?$": "ts-jest" },
+  testRegex: "/test/.*\\.(test|spec)?\\.(ts|tsx)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverageFrom: ["src/**/*.ts"],
+};
